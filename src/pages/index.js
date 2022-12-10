9e1a11f258f48d8048c5aba1dfe00b13a6e3d597
tejareddy1829 <tejar9700565670@gmail.com>
2022-12-10 13:12:21 +0530

    banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
@@ -7,7 +8,15 @@ export default function Home() {
       <Head>
         <title>Amazon 2.0</title>
       </Head>
+
       <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* banner */}
+        <Banner />
+
+        {/* productfeed */}
+      </main>
     </div>
   );
 }
