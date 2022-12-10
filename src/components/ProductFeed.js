35f2bd789922433352ef65457441d682f77d0889
tejareddy1829 <tejar9700565670@gmail.com>
2022-12-10 14:02:49 +0530

    product

---
+++ src/components/ProductFeed.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProductFeed() {
+  return (
+    <div>Product...</div>
+  )
+}
+
+export default ProductFeed
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,23 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-gray-100'>
       <Head>
         <title>Amazon 2.0</title>
       </Head>
 
       <Header />
 
-      <main className="max-w-screen-2xl mx-auto">
+      <main className='max-w-screen-2xl mx-auto'>
         {/* banner */}
         <Banner />
 
         {/* productfeed */}
+        <ProductFeed />
       </main>
     </div>
   );
