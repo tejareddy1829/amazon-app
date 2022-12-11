1a47ef74ff7f83c9dcba5f2a6b79fe2896719ec5
tejareddy1829 <tejar9700565670@gmail.com>
2022-12-11 16:33:05 +0530

    staricon

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import Image from "next/image";
+import {StarIcon} from "@heroicons/react/solid";
+
+const MAX_RATING = 5;
+const MIN_RATING = 1;
 
 function Product({id, title, description, price, category, image}) {
+  const [rating] = useState(
+    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+  );
+
+  const [hasPrime] = useState(Math.random() < 0.5);
+
   return (
     <div>
       <p>{category}</p>
@@ -9,6 +19,21 @@ function Product({id, title, description, price, category, image}) {
       <Image src={image} height={200} width={200} objectFit='contain' />
 
       <h4>{title}</h4>
+
+      <div className='flex'>
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon className='h-5' />
+          ))}
+      </div>
+
+      <p>{description}</p>
+
+    <div>
+        
+    </div>
+
     </div>
   );
 }
