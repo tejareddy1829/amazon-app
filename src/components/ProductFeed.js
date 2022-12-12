0a6412f2858dbb6e198223362c4ad4efe284a9d2
tejareddy1829 <tejar9700565670@gmail.com>
2022-12-12 12:51:37 +0530

    product-grid

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,19 +3,56 @@ import Product from "./Product";
 
 function ProductFeed({products}) {
   return (
-    <div>
-      <h1>Product...</h1>
-      {products.map(({id, title, description, price, category, image}) => (
-        <Product
-          key={id}
-          id={id}
-          title={title}
-          description={description}
-          price={price}
-          category={category}
-          image={image}
-        />
-      ))}
+    <div className='grid grid-flow-row-dense md: grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
+      {products
+        .slice(0, 4)
+        .map(({id, title, description, price, category, image}) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            description={description}
+            price={price}
+            category={category}
+            image={image}
+          />
+        ))}
+
+      <img
+        className='md:col-span-full'
+        src='https://links.papareact.com/dyz'
+        alt=''
+      />
+
+      <div className='col-span-2'>
+        {products
+          .slice(4, 5)
+          .map(({id, title, description, price, category, image}) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              description={description}
+              price={price}
+              category={category}
+              image={image}
+            />
+          ))}
+      </div>
+
+      {products
+          .slice(5, products.length)
+          .map(({id, title, description, price, category, image}) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              description={description}
+              price={price}
+              category={category}
+              image={image}
+            />
+          ))}
     </div>
   );
 }
