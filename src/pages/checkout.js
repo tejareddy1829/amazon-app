19514c6f3baff663e43227fce3e67be2831e68ae
tejareddy1829 <tejar9700565670@gmail.com>
2022-12-21 18:28:17 +0530

    sripe-axios

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -6,11 +6,23 @@ import {selectItems, selectTotal} from "../slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
 import Currency from "react-currency-formatter";
 import {useSession} from "next-auth/client";
+import {loadStripe} from "@stripe/stripe-js";
+import axios from "axios";
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function checkout() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
   const [session] = useSession();
+
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+
+    const checkoutSession = await axios.post("/api/create-checkout-session", {
+      items: items,
+      email: session.user.email,
+    });
+  };
 
   return (
     <div className='bg-gray-100'>
@@ -60,6 +72,7 @@ function checkout() {
 
               <button
                 role='link'
+                onClick={createCheckoutSession}
                 disabled={!session}
                 className={` button mt-2  ${!session && "sessionButton"} `}>
                 {!session ? "sign in to checkout" : "proceed to checkout"}
