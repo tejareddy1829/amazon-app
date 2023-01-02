92926b8fab802615d4d59ccb1c7defe8b3a6023c
tejareddy1829 <tejareddy292015@gmail.com>
2023-01-02 22:38:09 +0530

    webhook

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,7 +1,23 @@
 import {buffer} from "micro";
 import * as admin from "firebase-admin";
 
+const serviceAccount = require("../../../permissions.json");
+
+const app = !admin.apps.length
+  ? admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    })
+  : admin.app();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
 export default async (res, req) => {
   if (req.method === "POST") {
+    const requestBuffer = await buffer(req);
+    const payload = requestBuffer.toString();
+    const sig = req.headers["stripe-signature"];
+
+    let event;
   }
 };
