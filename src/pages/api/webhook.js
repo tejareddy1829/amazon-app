29d30a6d342548376be61ea06ee505fe9e9c93a4
tejareddy1829 <tejareddy292015@gmail.com>
2023-01-29 18:33:23 +0530

    webhook

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -19,12 +19,17 @@ const fulfillOrder = async (session) => {
     .firestore()
     .collection("users")
     .doc(session.metadata.email)
-    .collection("orders").doc(session.id).set({
+    .collection("orders")
+    .doc(session.id)
+    .set({
       amount: session.amount_total / 100,
       amount_shopping: session.total_details.amount_shipping / 100,
       images: JSON.parse(session.metadata.images),
-      timestamp: admin.firestore.FieldValue.serverTimestamp()
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
     })
+    .then(() => {
+      console.log(`success: order ${session.id} has been added to db`);
+    });
 };
 
 export default async (res, req) => {
@@ -44,6 +49,18 @@ export default async (res, req) => {
 
     if (event.type === "checkout.session.completed") {
       const session = event.data.object;
+
+      return fulfillOrder(session)
+        .then(() => res.status(200))
+        .catch(() => res.status(400))
+        .send(`webhook error: ${err.message}`);
     }
   }
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
 };
