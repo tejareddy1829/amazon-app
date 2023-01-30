3c2bdb7d4097e7450d07c16427458749a297a68c
tejareddy1829 <tejareddy292015@gmail.com>
2023-01-30 16:57:11 +0530

    success-button

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,7 +1,9 @@
-import {buffer} from "micro";
-import * as admin from "firebase-admin";
+import {buffer} from 'micro';
+import {doc, setDoc, serverTimestamp, getFirestore} from 'firebase/firestore';
+import * as admin from 'firebase-admin';
 
-const serviceAccount = require("../../../permissions.json");
+// secure a connection to firebase
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 const app = !admin.apps.length
   ? admin.initializeApp({
@@ -9,57 +11,65 @@ const app = !admin.apps.length
     })
   : admin.app();
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// establish a connection to stripe
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
 
+const db = getFirestore(app);
+
 const fulfillOrder = async (session) => {
-  return app
-    .firestore()
-    .collection("users")
-    .doc(session.metadata.email)
-    .collection("orders")
-    .doc(session.id)
-    .set({
-      amount: session.amount_total / 100,
-      amount_shopping: session.total_details.amount_shipping / 100,
-      images: JSON.parse(session.metadata.images),
-      timestamp: admin.firestore.FieldValue.serverTimestamp(),
-    })
-    .then(() => {
-      console.log(`success: order ${session.id} has been added to db`);
-    });
+  console.log('Fulfilling order', session);
+
+  const images = JSON.parse(session.metadata.images).map((image) =>
+    JSON.stringify(image)
+  );
+
+  const ref = doc(db, 'users', session.metadata.email, 'orders', session.id);
+
+  const refDoc = setDoc(ref, {
+    amount: session.amount_total / 100,
+    amount_shipping: session.total_details.amount_shipping / 100,
+    images: images,
+    timestamp: admin.serverTimestamp(),
+  })
+    .then(() => console.log(`SUCCESS`))
+    .catch((err) => console.log('Error:', err.message));
+
+  return refDoc;
 };
 
-export default async (res, req) => {
-  if (req.method === "POST") {
+const final = async (req, res) => {
+  if (req.method === 'POST') {
     const requestBuffer = await buffer(req);
     const payload = requestBuffer.toString();
-    const sig = req.headers["stripe-signature"];
-
+    const sig = req.headers['stripe-signature'];
     let event;
-    console.log("POST");
+    // verifying if event came from stripe
     try {
       event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
     } catch (err) {
-      console.log("ERROR", err.message);
-      return res.status(400).send(`webhook error: ${err.message}`);
+      console.log('Error', err.message);
+      return res.status(400).send(`Webhook error: ${err.message}`);
     }
 
-    if (event.type === "checkout.session.completed") {
+    // handle checkout session completed event
+    if (event.type === 'checkout.session.completed') {
+      console.log('Session completed success');
       const session = event.data.object;
 
+      // fulfilling the order
       return fulfillOrder(session)
         .then(() => res.status(200))
-        .catch((e) => console.log({e}))
-        .send(`webhook error: ${err.message}`);
+        .catch((err) => res.status(400).send(`Webhook Error: ${err.message}`));
     }
   }
 };
 
+export default final;
+
 export const config = {
   api: {
     bodyParser: false,
-    externalResolver: true,
   },
 };
--- src/pages/success.js
+++ src/pages/success.js
@@ -1,12 +1,34 @@
-import React from "react";
-import Header from "../components/Header";
+import {CheckCircleIcon} from '@heroicons/react/outline';
+import {useRouter} from 'next/router';
+import React from 'react';
+import Header from '../components/Header';
 
 function success() {
+  const router = useRouter();
   return (
     <div className='bg-gray-100 h-screen'>
       <Header />
-      
-      <main></main>
+
+      <main className='max-w-screen-lg mx-auto'>
+        <div className='flex items-center flex-col p-10 bg-white'>
+          <div className='flex items-center space-x-2 mb-5'>
+            <CheckCircleIcon className='text-green-500 h-10' />
+            <h1 className='text-3xl'>
+              Thank You, Your order has been confirmed!
+            </h1>
+          </div>
+          <p>
+            Thank you for shopping with us. we'll send a confirmation of ordered
+            shipped, if you like to check the status of the order please click
+            the link below.
+          </p>
+          <button
+            onClick={() => router.push('/orders')}
+            className='button mt-8'>
+            Go to my orders
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
